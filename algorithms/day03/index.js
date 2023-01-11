f6046c16a98fac67d947f709459d286596316465
tmax818 <tylermaxwell661@gmail.com>
2023-01-11 07:57:44 -0800

    update algo

---
+++ algorithms/day03/index.js
@@ -0,0 +1,33 @@
+/* 
+  String: Reverse
+  Given a string,
+  return a new string that is the given string reversed
+*/
+
+const str1 = "creature";
+const expected1 = "erutaerc";
+
+const str2 = "dog";
+const expected2 = "god";
+
+const str3 = "hello";
+const expected3 = "olleh";
+
+const str4 = "";
+const expected4 = "";
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+function reverseString(str) {
+
+}
+
+console.log(reverseString(str1))
+console.log(reverseString(str2))
+console.log(reverseString(str3))
+console.log(reverseString(str4))
