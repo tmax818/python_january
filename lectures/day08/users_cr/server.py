c66260927123ed7e9db424f99d43824c10a505c4
tmax818 <tylermaxwell661@gmail.com>
2023-01-18 12:17:55 -0800

    remove comments

--- lectures/day08/users_cr/server.py
+++ lectures/day08/users_cr/server.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
-# import the class from user.py
 from user import User
+
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-    # call the get all classmethod to get all users
     users = User.get_all()
     print(users)
     return render_template("index.html", users = users)
             
+            
+
 if __name__ == "__main__":
     app.run(debug=True)
--- lectures/day08/users_cr/user.py
+++ lectures/day08/users_cr/user.py
@@ -1,7 +1,6 @@
-# import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 from pprint import pprint
-# model the class after the user table from our database
+
 
 DATABASE = 'users_cr'
 
@@ -13,16 +12,13 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL(DATABASE).query_db(query)
         pprint(results)
-        # Create an empty list to append our instances of users
         users = []
-        # Iterate over the db results and create instances of users with cls.
         for user in results:
             users.append( User(user) )
         return users
