178e65829e5043b7aceabbc584807c440fcddff2
tmax818 <tylermaxwell661@gmail.com>
2023-01-18 14:25:45 -0800

    update

--- lectures/day08/users_cr/server.py
+++ lectures/day08/users_cr/server.py
@@ -1,15 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from user import User
 
 app = Flask(__name__)
 
+# ! CREATE
+
+@app.route('/users/new')
+def new_user():
+    return render_template("new.html")
+
+@app.route('/user/create', methods=["POST"])
+def create_user():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/')
+    
+
+# ! READ ALL
 @app.route("/")
 def index():
-    users = User.get_all()
-    print(users)
-    return render_template("index.html", users = users)
-            
-            
+    return render_template("index.html", users = User.get_all())
+    
 
+# ! READ ONE
+
+@app.route("/users/<int:id>")
+def get_user(id):
+    data = {'id': id}
+    
+    return render_template('show.html', user = User.get_one(data))
+    
+
+#  ! UPDATE
+
+@app.route("/users/edit/<int:id>")
+def edit(id):
+    data = {'id': id}   
+    return render_template('edit.html', user = User.get_one(data))
+
+@app.route('/user/update', methods=["POST"])
+def update():
+    User.update(request.form)
+    return redirect('/')
+
+# ! DELETE        
+            
 if __name__ == "__main__":
     app.run(debug=True)
--- lectures/day08/users_cr/user.py
+++ lectures/day08/users_cr/user.py
@@ -12,7 +12,13 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-
+    # ! CREATE
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s,%(last_name)s, %(email)s);"
+        return connectToMySQL(DATABASE).query_db(query, data)
+    
+    #  ! READ ALL
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -22,3 +28,24 @@ class User:
         for user in results:
             users.append( User(user) )
         return users
+    
+    # ! READ ONE
+    
+    @classmethod
+    def get_one(cls,data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL(DATABASE).query_db(query,data)
+        pprint(result[0])
+        user = User(result[0])
+        print(user)
+        return user
+        
+    
+    #  ! UPDATE
+    
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+        return connectToMySQL(DATABASE).query_db(query,data)
+    
+    # ! DELETE
