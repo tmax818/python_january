0898bb4ac12da39030d699fdc385a5789a8f1396
tmax818 <tylermaxwell661@gmail.com>
2023-01-23 15:05:08 -0800

    update

--- lectures/day11/recipes/flask_app/controllers/recipes.py
+++ lectures/day11/recipes/flask_app/controllers/recipes.py
@@ -5,7 +5,7 @@ from flask_app.models.recipe import Recipe
 def recipes():
     if 'user_id' not in session:
         return redirect('/logout')
-    return render_template('recipes.html')
+    return render_template('recipes.html', recipes = Recipe.get_all())
 
 @app.route('/recipes/new')
 def new_recipe():
@@ -14,6 +14,19 @@ def new_recipe():
 @app.route('/recipes/create', methods = ['POST'])
 def create_recipe():
     print(request.form)
+    if not Recipe.validate_recipe(request.form):
+        return redirect('/recipes/new')
     Recipe.save(request.form)
     return redirect('/recipes')
+
+@app.route('/recipes/<int:id>')
+def show_recipe(id):
+    data = {'id': id}
+    return render_template("show_recipe.html", recipe = Recipe.get_one(data))
+
+@app.route("/recipes/edit/<int:id>")
+def edit_recipe(id):
+    data = {'id': id}
+    return render_template("edit_recipe.html", recipe = Recipe.get_one(data))
+    
     
--- lectures/day11/recipes/flask_app/models/recipe.py
+++ lectures/day11/recipes/flask_app/models/recipe.py
@@ -1,5 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-
+from flask_app import flash
+from pprint import pprint
 DATABASE = 'recipes'
 
 class Recipe:
@@ -12,6 +13,7 @@ class Recipe:
         self.date_made = data['date_made']
         self.under_30 = data['under_30']
         self.user_id = data['user_id']
+        self.first_name = data['first_name']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         
@@ -19,3 +21,28 @@ class Recipe:
     def save(cls, data):
         query = "INSERT INTO recipes (name, description, instructions, date_made, under_30, user_id) VALUES (%(name)s, %(description)s, %(instructions)s, %(date_made)s, %(under_30)s, %(user_id)s)"
         return connectToMySQL(DATABASE).query_db(query, data)
+    
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM recipes JOIN users ON users.id = recipes.user_id;"
+        results = connectToMySQL(DATABASE).query_db(query)
+        pprint(results)
+        recipes = []
+        for recipe in results:
+            recipes.append(cls(recipe))
+        return recipes
+    
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM recipes JOIN users ON users.id = recipes.user_id WHERE recipes.id = %(id)s;"
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        pprint(result)
+        return Recipe(result)
+        
+    @staticmethod
+    def validate_recipe(recipe):
+        is_valid = True
+        if len(recipe['name']) < 3:
+            flash("Name must be three chars")
+            is_valid = False
+        return is_valid
