84e4a7b6655fd80040e86580a35ea09480d4b1fb
tmax818 <tylermaxwell661@gmail.com>
2023-01-26 07:02:20 -0800

    add algo

---
+++ algorithms/day10/index.js
@@ -0,0 +1,26 @@
+/* 
+  Given a string,
+  return a new string with the duplicates excluded
+  Bonus: Keep only the last instance of each character.
+*/
+
+const str1 = "abcABC";
+const expected1 = "abcABC";
+
+const str2 = "helloo";
+const expected2 = "helo";
+
+const str3 = "";
+const expected3 = "";
+
+const str4 = "aa";
+const expected4 = "a";
+
+/**
+ * De-dupes the given string.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str A string that may contain duplicates.
+ * @returns {string} The given string with any duplicate characters removed.
+ */
+function stringDedupe(str) {}
