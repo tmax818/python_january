80a7681b3450913bb1d4e3246a9449e90703b42f
tmax818 <tylermaxwell661@gmail.com>
2023-01-27 07:07:14 -0800

    add algo

---
+++ algorithms/day12/index.js
@@ -0,0 +1,30 @@
+/* 
+  Given an int to represent how much change is needed
+  calculate the fewest number of coins needed to create that change,
+  using the standard US denominations
+*/
+
+const cents1 = 25;
+const expected1 = { quarter: 1 };
+
+const cents2 = 50;
+const expected2 = { quarter: 2 };
+
+const cents3 = 9;
+const expected3 = { nickel: 1, penny: 4 };
+
+const cents4 = 99;
+const expected4 = { quarter: 3, dime: 2, penny: 4 };
+
+/**
+ * Calculates the fewest coins of the standard American denominations needed
+ *    to reach the given cents amount.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} cents
+ * @param {string} sick
+ * @returns {Object<string, number>} - A denomination table where the keys are
+ *    denomination names and the value is the amount of that denomination
+ *    needed.
+ */
+function fewestCoinChange(cents) {}
