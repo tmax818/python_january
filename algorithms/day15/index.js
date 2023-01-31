7c5bb78be4882f4580361044e54cb257e772f092
tmax818 <tylermaxwell661@gmail.com>
2023-01-31 07:55:53 -0800

    add algo

---
+++ algorithms/day15/index.js
@@ -0,0 +1,30 @@
+/* 
+Recursive Sigma
+Input: integer
+Output: sum of integers from 1 to Input integer
+*/
+
+const num1 = 5;
+const expected1 = 15;
+// Explanation: (1+2+3+4+5)
+
+const num2 = 2.5;
+const expected2 = 3;
+// Explanation: (1+2)
+
+const num3 = -1;
+const expected3 = 0;
+
+/**
+ * Recursively sum the given int and every previous positive int.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} num
+ * @returns {number}
+ */
+function recursiveSigma(num) {
+}
+
+console.log(recursiveSigma(num1))
+console.log(recursiveSigma(num2))
+console.log(recursiveSigma(num3))
---
+++ algorithms/day15/sol.js
@@ -0,0 +1,37 @@
+/* 
+Recursive Sigma
+Input: integer
+Output: sum of integers from 1 to Input integer
+*/
+
+const num1 = 5;
+const expected1 = 15;
+// Explanation: (1+2+3+4+5)
+
+const num2 = 2.5;
+const expected2 = 3;
+// Explanation: (1+2)
+
+const num3 = -1;
+const expected3 = 0;
+
+/**
+ * Recursively sum the given int and every previous positive int.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} num
+ * @returns {number}
+ */
+function recursiveSigma(num) {
+  // get rid of decimals
+  const int = parseInt(num);
+  // base case
+  if (int < 1) {
+    return 0;
+  }
+  return int + recursiveSigma(int - 1);
+}
+
+console.log(recursiveSigma(num1))
+console.log(recursiveSigma(num2))
+console.log(recursiveSigma(num3))
