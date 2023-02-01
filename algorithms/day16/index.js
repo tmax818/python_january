7014b3f2f36157c4284398957b6731f1e94f97dd
tmax818 <tylermaxwell661@gmail.com>
2023-02-01 07:32:07 -0800

    add algo

---
+++ algorithms/day16/index.js
@@ -0,0 +1,24 @@
+/* 
+  Recursively sum an arr of ints
+*/
+
+const nums1 = [1, 2, 3];
+const expected1 = 6;
+
+const nums2 = [1];
+const expected2 = 1;
+
+const nums3 = [];
+const expected3 = 0;
+
+/**
+ * Add params if needed for recursion
+ * Recursively sums the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {number} The sum of the given nums.
+ */
+function sumArr(nums) {}
+
+/*****************************************************************************/
